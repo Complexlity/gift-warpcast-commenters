6fd1de0cc9649eff057f9db6fd699f0b36ba0183
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-22 17:15:39 +0100

    feat: complete scripts

---
+++ README.md
@@ -0,0 +1,60 @@
+# Tip Commenters on a specified cast
+This contains two scripts that enables tipping people that comment on a warpcast cast
+
+## Pre-requisites
+- [node/npm](https://nodejs.org/en) installed on your machine. The version used for testing is 18.7.1 but should work with newer nodejs version as well
+- Private key of a wallet containing some etheruem (for cast) and [DEGEN](https://www.coingecko.com/en/coins/degen) tokens
+
+
+## Getting Started
+- Clone this repo locally
+```
+git clone https://github.com/Complexlity/gift-warpcast-commenters.git
+```
+
+- Enter the directory
+```
+cd gift-warpcast-commenters
+```
+
+- Install dependencies
+```
+npm install
+or
+pnpm install
+```
+
+- Add Environmental Variables
+```
+#.env
+# Your address private key
+PRIVATE_KEY=
+# The default name for the output file
+DEFAULT_WINNERS_FILE=winners.csv
+# Remove this if you want to also gift the thread owner
+EXCLUDE_THREAD_OWNER=true
+```
+
+Go into the `.env` file and input you wallet private key.
+The other two variables are optional
+
+## How to Use
+1. Get Replies For A warpcast Url
+```
+node getReplies url output_file
+```
+
+`output_file` is optional and defaults to the `DEFAULT_WINNERS_FILE` in the .env
+
+e.g `node getReplies https://warpcast.com/complexlity/0x2213bfa1 commenters.csv`
+
+2. Gift DEGENS to the commenters
+Currently, only degen is supported
+```
+node giftTokens amount input_file
+```
+
+`input_file` is optional and defaults to the `DEFAULT_WINNERS_FILE` in the .env
+
+e.g `node giftTokens 100 commenters.csv`
+
---
+++ giftTokens.js
@@ -0,0 +1,91 @@
+import { ethers, NonceManager } from "ethers";
+import fs from 'fs'
+
+import { config } from "dotenv"
+config()
+
+function isNumber(inputAmount) {
+    const amount = Number(inputAmount)
+    return !isNaN(amount);
+  }
+
+const NUMBER_OF_TOKENS = process.argv[2]
+if (!isNumber(NUMBER_OF_TOKENS)) {
+  throw new Error(
+    "Enter the number of token you want to gift e.g e.g node giftTokens.js 100"
+  );
+}
+
+if (Number(NUMBER_OF_TOKENS) < 1) {
+  throw new Error("number of tokens must be greater that 0")
+}
+
+const inputFile = process.argv[3] ?? process.env.DEFAULT_WINNERS_FILE;
+if (!inputFile) {
+  throw new Error("You need an input file with address e.g. node giftTokens.js 100 winners.csv")
+}
+
+
+
+// Degen is the default type
+const CURRENT_TOKEN = 'DEGEN'
+const BASE_MAINNET_RPC = "https://mainnet.base.org/";
+const DEGEN_CONTRACT_ADDRESS = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed";
+
+
+/*
+Data structure to make this composable and accept multiple token types in future
+*/
+const tokens = {
+  DEGEN: {
+    mainnetRPC: BASE_MAINNET_RPC,
+    contractAddress: DEGEN_CONTRACT_ADDRESS,
+  },
+};
+
+const tokenDetails = tokens[CURRENT_TOKEN]
+const addresses = fs.readFileSync(inputFile, 'utf-8')
+const recepientss = addresses.split()
+// console.log({ recepients: recepientss })
+console.log({ amount: NUMBER_OF_TOKENS, token: CURRENT_TOKEN})
+
+// Account 4
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+if (!PRIVATE_KEY) {
+  throw new Error("Add you account private key to .env")
+}
+
+const provider = new ethers.JsonRpcProvider(tokenDetails.mainnetRPC);
+
+const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+const abi = [
+  "function decimals() view returns (uint8)",
+  "function symbol() view returns (string)",
+  "function balanceOf(address addr) view returns (uint)",
+  "function transfer(address to, uint amount)",
+];
+
+console.log("Initializing");
+/* Test you have the right token address;
+const symbol = await tokenContractWithProvider.symbol();
+console.log({ symbol })
+// The symbol should be the token symbol. e.g 'DEGEN'
+*/
+const tokenContractWithSigner = new ethers.Contract(
+  tokenDetails.contractAddress,
+  abi,
+  signer
+);
+
+
+const amount = ethers.parseUnits(NUMBER_OF_TOKENS);
+for await (const address of recepients) {
+  console.log(`Sending ${NUMBER_OF_TOKENS} ${CURRENT_TOKEN} to ${address}`)
+  const result = await tokenContractWithSigner.transfer(address, amount).then(tx => tx.wait())
+  console.log(
+    "Successful. View on Explorer",
+    `https://basescan.org/tx/${result.hash}`
+  );
+}
+
